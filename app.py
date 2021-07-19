c1b0b602258dd51d1b491b8c97021f22a9cde20a
Rachel Wyatt <wyattr@gmail.com>
2021-07-18 22:54:44 -0400

    trying to deploy again

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Run this app with `python dashboard.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
-
+import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
