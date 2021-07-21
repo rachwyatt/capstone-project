911aa674870c9ff66332f97758022b739b518832
Ai Zhong <aizhong.ds@gmail.com>
2021-07-21 16:06:34 -0700

    add in data processing files

---
+++ data_processing/jd_parse_utils.py
@@ -0,0 +1,165 @@
+import pandas as pd
+
+
+def load_skill_dict(path='skillsets.csv'):
+    skills = pd.read_csv(path)
+    skills['Skills'] = skills['Skills'].str.lower()
+    skills['Category'] = skills['Category'].str.lower()
+    skills_dict = skills.set_index('Skills').to_dict()
+    skills_dict = skills_dict['Category']
+    return skills_dict
+
+
+def get_skills(text):
+    import regex as ree
+
+    skills_dict = load_skill_dict()
+    skills_ls = list(skills_dict.keys())
+    
+    p1 = ree.compile(r" \L<words> ", words=skills_ls)
+    p2 = ree.compile(r"\t\L<words> ", words=skills_ls)
+    p3 = ree.compile(r",\L<words> ", words=skills_ls)
+    p4 = ree.compile(r" \L<words>,", words=skills_ls)
+    p5 = ree.compile(r" \L<words>\.", words=skills_ls)
+    p6 = ree.compile(r" \L<words>\n", words=skills_ls)
+    p7 = ree.compile(r" \L<words>/", words=skills_ls)
+    p8 = ree.compile(r"/\L<words>", words=skills_ls)
+    p9 = ree.compile(r"\(\L<words>", words=skills_ls)
+    p10 = ree.compile(r" \L<words>\)", words=skills_ls)
+    p11 = ree.compile(r"\n\L<words>", words=skills_ls)
+    patterns=[p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11]
+
+    found_skills=[]
+    for p in patterns:
+        found_skills += p.findall(text.lower())
+        
+    found_skills = [x.strip(' ,\.\t\n/)(') for x in found_skills]
+    
+    # get the unique skills
+    found_skills = sorted(list(set(found_skills)))
+    return found_skills
+
+
+def get_skill_cat(skill_ls):
+    cats = []
+    skills_dict = load_skill_dict()
+    skills_ls = list(skills_dict.keys())
+
+    for sk in skill_ls:
+        try:
+            cat = skills_dict[sk]
+            cats.append(cat)
+        except:
+            cats.append(skills_dict['.net'])
+    return cats
+
+
+
+def degree_names():
+    list_of_postdoc_names = (
+        "Postdoc", 
+        "Postdoctoral Researcher",
+        "Research Scientist"
+    )
+
+    list_of_phd_names = (
+        "PhD",
+        "Ph.D.",
+        "Doctorate"
+    )
+
+    list_of_master_names = (
+        "MSc",
+        "Master ",
+        "Masters",
+        "M.Sc.",
+        "M.S.",
+        " MS,",
+        " MS of ",
+        " MS in ",
+        " MA ",
+        " MA,",
+        "M.A.",
+        "M.B.A.",
+        " MBA ",
+        "Master of ",
+        "Post Graduate Diploma",
+        "/MS/",
+        "MS/",
+    )
+
+    list_of_bs_names = (
+        "B.S.",
+        " BS,"
+        "B.S ",
+        " BS ",
+        "BS/",
+        "/BS/",
+        " BA ",
+        "B.A.,",
+        "B.A.-",
+        "B.A. ",
+        "Bachelor of Science",
+        "Bachelor of"
+        "B.Sc.",
+        "Bachelor",
+        "B.E. ",
+        "B.E.,",
+        "B.E.-",
+        " BSE ",
+        "Bachelor of Engineering",
+        "B.Tech ",
+        "B.Tech,",
+        "B.Tech-"   
+    )
+
+    return (list_of_postdoc_names, 
+         list_of_phd_names,
+         list_of_master_names,
+         list_of_bs_names)
+
+
+
+def clean_deg(x, keyword = 'bachelor'):
+    if len(x)>0:
+        return keyword
+    else:
+        return ""
+
+
+def get_skill_education(jd):
+	"""
+	extract education and skills requirements from the job description
+
+	return education requirements and skills
+	"""
+    import regex as ree
+    jd = jd.lower()
+    skills = get_skills(jd)
+	# posdoc, phd, master, bs
+    degree_list_of_lists = degree_names()
+    found_items = []
+    for ind, ls in enumerate(degree_list_of_lists):
+        p = ree.compile(r"\L<words>", words=[kk.lower() for kk in ls])
+        found = p.findall(jd)
+        if len(found)>1:
+            if ind==0:
+                found_items.append('posdoc')
+            elif ind==1:
+                found_items.append('phd')
+            elif ind==2:
+                found_items.append('master')
+            elif ind==3:
+                found_items.append('bs')
+
+    education = ';'.join(found_items)
+    skills = ', '.join(skills)
+
+    return education, skills
+
+
+
+
+
+
+
---
+++ data_processing/jd_scraping.py
@@ -0,0 +1,253 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+from selenium import webdriver
+import chromedriver_autoinstaller
+from tqdm import tqdm
+import pymysql.cursors
+import pandas as pd
+from datetime import datetime
+import json 
+from jd_parse_utils import get_skill_education
+
+
+chromedriver_autoinstaller.install() 
+today = str(datetime.now().date())
+
+
+def load_titles(jobtitles = 'jobtitles_ds.txt'):
+	with open(jobtitles, 'r') as f:
+        titles = f.readlines()
+	titles = [x.lower().strip().replace(' ','%20') for x in titles]
+	return titles 
+
+
+def scrape_parent_links():
+	print('Scraping job pages links...')
+	joblinks_batch=[]
+	titles = load_titles()
+	## this code only deals with 30 pages for each query (seems that's what glassdoor release)
+	for keyword in tqdm(titles):    
+	    link = f'https://www.glassdoor.com/Job/jobs.htm?sc.keyword={keyword}&fromAge=1'
+	    chrome_options = webdriver.ChromeOptions()
+	    # chrome_options.add_argument('--headless')
+	    chrome_options.add_argument('--no-sandbox')
+	    chrome_options.add_argument('--disable-dev-shm-usage')
+
+	    wd = webdriver.Chrome('chromedriver', options=chrome_options)
+	    wd.get(link)
+	    raw = wd.page_source
+	    soup = BeautifulSoup(raw,'lxml')
+	    joblinks = list(set([x['href'] for x in soup.find_all('a',{'class':'jobLink'})]))
+	    while True:
+	        try:
+	            wd.find_element_by_xpath('//*[@id="FooterPageNav"]/div/ul/li[7]/a').click()
+	            time.sleep(4)
+
+	            for l in BeautifulSoup(wd.page_source,'lxml').find_all('a'):
+	                try:
+	                    link=(l['href'])
+	                    if link.startswith('/partner/'):
+	                        joblinks.append(link)
+	                except:
+	                    continue
+
+	            joblinks = list(set(joblinks))
+
+	        except:
+	            break
+	            wd.close()
+	            
+	    wd.close()
+	    joblinks_batch+=joblinks
+
+	return joblinks_batch
+
+
+def glassdoor_clean(soup):
+    cleaned = {}
+    
+    parsed = []
+    for x in soup.find_all('script'):
+        try:
+            parsed.append(json.loads(x.text))
+        except:
+            try:
+                temp = str(x).split('<script type="application/ld+json">')[-1].split('</script>')[0]
+                temp = json.loads(temp)
+                parsed.append(temp)
+            except:
+                continue
+    
+    if not parsed:
+        return {}
+      
+    try:
+        title = parsed[0]['title']
+        company = parsed[0]['hiringOrganization']['name']
+        addr = parsed[0]['jobLocation']['address']
+        try:
+            location = str(addr['addressLocality'])+', '+str(addr['addressRegion'])
+        except:
+            location = str(addr['addressRegion'])
+    except:
+        title = ''
+        company = ''
+        location = ''
+        
+    raw = parsed[0]
+    try:
+        jd = BeautifulSoup(parsed[0]['description'], 'lxml').text
+    except:
+        jd = parse_jd(soup)
+
+    cleaned['title']=title
+    cleaned['company']=company
+    cleaned['location']=location
+    cleaned['jd']=jd
+    cleaned['meta']=raw
+    
+    return cleaned
+
+
+def parse_jd(soup):
+    jd = soup.find('div',{'id':'JobDescriptionContainer'})
+    
+    children=[]
+    for ch in jd.findChildren(recursive=False):
+        for ch2 in ch.findChildren(recursive=False):
+            ch3=ch2.findChildren(recursive=False)
+            if ch3:
+                for x in ch3:
+                    children+=x
+            else:
+                children+=ch2
+
+    elements=[]
+    for n,ch in enumerate(children):
+        try:
+            sub=ch.find_all('li')
+            if len(sub)>1:
+                elements+=sub
+            else:
+                elements.append(ch)
+        except:
+            elements.append(ch)
+
+    jd_cleaned = []
+    for x in elements:
+        try:
+            txt = x.text.strip()
+            if txt!='':
+                jd_cleaned.append(txt)
+        except:
+            jd_cleaned.append(str(x).strip())
+
+    jd_cleaned = '\n'.join(jd_cleaned)
+    
+    return jd_cleaned
+
+
+def scrape_job_page(joblinks_batch):
+
+	print('Scraping each job pages...')
+	cleaned_dicts = []
+
+    chrome_options2 = webdriver.ChromeOptions()
+    chrome_options2.add_argument('--headless')
+    chrome_options2.add_argument('--no-sandbox')
+    chrome_options2.add_argument('--disable-dev-shm-usage')
+
+    for l in tqdm(joblinks_batch):   
+        wd2 = webdriver.Chrome('chromedriver', options=chrome_options2)
+        wd2.get("https://www.glassdoor.com"+l)
+        raw2 = wd2.page_source
+        cleaned = glassdoor_clean(BeautifulSoup(raw2, 'lxml'))
+        cleaned_dicts.append(cleaned)
+        wd2.close()
+    
+    cleaned_dicts = [x for x in cleaned_dicts if x!={}]
+    cleaned = pd.DataFrame(cleaned_dicts)
+	cleaned.columns = ['job_title','company_name','location','job_description','meta']
+
+	metas = cleaned['meta'].tolist()
+
+	date_posted=[]
+	job_types=[]
+	urls=[]
+
+	for meta in metas:
+	    try:
+	        d1=meta['datePosted']
+	    except:
+	        d1 = now
+	    date_posted.append(d1)
+	    try:
+	        d2=meta['employmentType']
+	    except:
+	        d2 = ''
+	    job_types.append(d2)
+	    try:
+	        d3=meta['url']
+	    except:
+	        d3=''
+	    urls.append(d3)
+
+	cleaned['url']=urls
+	cleaned['job_type']=job_types
+	cleaned['date_posted']=date_posted
+	cleaned = cleaned[cleaned['url']!='']
+	df = cleaned.fillna('')
+	df['crawl_timestamp'] = str(today)
+	df = df.rename(columns={'date_posted':'post_date'})
+	df['job_board']='glassdoor'
+
+	edu=[]
+	skill=[]
+	for jd in df['job_description'].tolist():
+		e, s = get_skill_education(jd)
+		edu.append(e)
+		skill.append(s)
+
+	df['skill'] = skill
+	df['education'] = edu
+
+    return df
+
+
+def insert_into_db(df):
+    
+    print('Inserting into DB...')
+
+	connection = pymysql.connect(host="job-market.chfeqjbmewii.us-west-1.rds.amazonaws.com",user="root",
+	                             password="mads_capstone",database="capstone",port=3306,
+	                             charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+	cursor = connection.cursor()
+
+	sql = """INSERT INTO jd (crawl_timestamp, url, job_title, company_name,
+	post_date, job_description, job_type, job_board, location, skill, education) 
+	VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+
+	dup_cnt=0
+	commit_cnt=0
+	for i in tqdm(range(len(df))):
+	    try:
+	        data=df.iloc[i]
+	        cursor.execute(sql, (data['crawl_timestamp'], data['url'], data['job_title'],
+	                             data['company_name'],data['post_date'], data['job_description'],
+	                             data['job_type'],data['job_board'], data['location'], data['skill'], 
+	                             data['education']))
+	        connection.commit()
+	        commit_cnt+=1
+	    except pymysql.IntegrityError as err:
+	        dup_cnt+=1
+
+	connection.close()
+
+	print('=====> newly commited entries:', commit_cnt, '\nduplicated entries:', dup_cnt)
+
+
+def main():
+	job_links_batch = scrape_parent_links()
+	df = scrape_job_page(joblinks_batch)
+	insert_into_db(df)
