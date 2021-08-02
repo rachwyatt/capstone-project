8f63c68b45722e5dc1e3350a4230b9b83b3e4e7a
Rachel Wyatt <wyattr@gmail.com>
2021-08-02 01:37:57 -0400

    connect database to django app

--- website/models.py
+++ website/models.py
@@ -1,3 +1,48 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+class Companies(models.Model):
+    name = models.CharField(max_length=500, blank=True, null=True)
+    domain = models.CharField(max_length=500)
+    year_founded = models.IntegerField(blank=True, null=True)
+    industry = models.CharField(max_length=500, blank=True, null=True)
+    size_range = models.CharField(max_length=45, blank=True, null=True)
+    country = models.CharField(max_length=45, blank=True, null=True)
+    linkedin_url = models.CharField(unique=True, max_length=500, blank=True, null=True)
+    current_employee_est = models.IntegerField(blank=True, null=True)
+    total_employee_est = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'companies'
+
+class Jd(models.Model):
+    crawl_timestamp = models.CharField(max_length=100)
+    url = models.CharField(unique=True, max_length=600, blank=True, null=True)
+    job_title = models.CharField(max_length=500)
+    category = models.CharField(max_length=45, blank=True, null=True)
+    company_name = models.CharField(max_length=500, blank=True, null=True)
+    city = models.CharField(max_length=45, blank=True, null=True)
+    state = models.CharField(max_length=45, blank=True, null=True)
+    country = models.CharField(max_length=45, blank=True, null=True)
+    post_date = models.CharField(max_length=45, blank=True, null=True)
+    job_description = models.TextField()
+    job_type = models.CharField(max_length=45, blank=True, null=True)
+    job_board = models.CharField(max_length=45, blank=True, null=True)
+    sector = models.CharField(max_length=45, blank=True, null=True)
+    html_job_description = models.TextField(blank=True, null=True)
+    company_description = models.TextField(blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    education = models.TextField(blank=True, null=True)
+    skill = models.TextField(blank=True, null=True)
+    cleaned_jd = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'jd'
